4da49628307549d4218b61cfb1251df9413647bd
Ahmad Alinaghian <93351358+ahmadad62@users.noreply.github.com>
2023-06-25 20:26:17 +0200

    Update HelloClass.java

--- Ahmad/src/aits/HelloClass.java
+++ Ahmad/src/aits/HelloClass.java
@@ -18,7 +18,17 @@ public class HelloClass {
 	System.out.println("Hello "+name);
 	System.out.println("Your age "+age+" years old");
 	System.out.println("You like "+food);
+	System.out.println("what is your name?");
+	String name=scanner.nextLine();
+	System.out.println("How old are you?");
+	int age=scanner.nextInt();
+	scanner.nextLine();
+	System.out.println("What is favorite food?");
+	String food=scanner.nextLine();
 	
+	System.out.println("Hello "+name);
+	System.out.println("Your age "+age+" years old");
+	System.out.println("You like "+food);
 	}
 
 }
